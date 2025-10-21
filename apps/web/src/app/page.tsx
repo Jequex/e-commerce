6d9fe313becc5ee99489e35261f2015d90220ed0
/Jequex <onyejekwe.harddisk20@gmail.com>
2025-10-21 21:45:46 +0100

    links updated

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,5 +1,7 @@
+import { routing } from "@/i18n/routing";
 import { redirect } from "next/navigation";
 
 export default function RootPage() {
-  redirect("/en");
+  // Redirect to default locale - middleware will handle locale detection
+  redirect(`/${routing.defaultLocale}`);
 }
